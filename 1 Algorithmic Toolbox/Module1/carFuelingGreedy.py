d74c4f1ecdbfc46225e992a0455fe8c349dd4c13
sammylton <sameerbaheti84@gmail.com>
2026-03-03 07:39:43 +0530

    refactor: include typesetting and improve docstring in greedy car fueling code

--- 1 Algorithmic Toolbox/Module1/carFuelingGreedy.py
+++ 1 Algorithmic Toolbox/Module1/carFuelingGreedy.py
@@ -1,19 +1,24 @@
-def minRefills(x, n, L):
+from typing import List
+
+def minRefills(x: List[float], n: int, L: float) -> int:
     """
-    x (array): array of gas station distances including start and end points
+    Args:
+    x (list): list of gas station distances including start and end points
     n (int): number of gas stations
     L (int): driving range (on a full tank)
+    Returns:
+    int: number of gas station stops/refills
     """
     numRefills = 0
     currStop = 0
     lastStop = 0
-    print("C", "L", "N")
+    # print("C", "L", "N") # check
     while currStop < n+1:
         currStop += 1
         if x[currStop] - x[lastStop] > L:
             numRefills += 1
             lastStop = currStop - 1
-        print(currStop, lastStop, numRefills)
+        # print(currStop, lastStop, numRefills) # check
     return numRefills
 
 if __name__ == "__main__":
